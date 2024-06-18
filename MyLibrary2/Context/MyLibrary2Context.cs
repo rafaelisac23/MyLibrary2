7f5f0196122efb956b6d6cb6f6ae9045a1219b56
rafael isac <rafael.darrigo@gmail.com>
2024-06-18 19:48:44 -0300

    ascionandoBanco

---
+++ MyLibrary2/Context/MyLibrary2Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyLibrary2.Context
+{
+    public class MyLibrary2Context : DbContext
+    {
+
+        public MyLibrary2Context(DbContextOptions<MyLibrary2Context>options) : base(options){}
+    }
+}
